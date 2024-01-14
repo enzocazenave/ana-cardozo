841f66c6a7d8c82091e9d4dde431aec497b09493
Enzo Cazenave <encaze24@hotmail.com>
2024-01-14 11:05:44 -0300

    Add responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      '1440': '1440px',
+      '1366': '1366px',
+      '1280': '1280px',
+      '1024': '1024px',
+      '800': '800px',
+      '520': '520px'
+    },
     extend: {
       colors: {
         'light-pink': '#FFE6E9',
